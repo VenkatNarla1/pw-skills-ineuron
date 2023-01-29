3c9a03485a8e2f25b84c933c86c809cb30189170
VenkatNarla1 <104138656+VenkatNarla1@users.noreply.github.com>
2023-01-29 13:24:02 +0530

    Add files via upload

---
+++ AI-object Remover/object-remover.py
@@ -0,0 +1,157 @@
+!git clone https://github.com/saic-mdal/lama.git
+
+!pip install torch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 torchtext==0.9
+!pip install -r lama/requirements.txt --quiet
+!pip install wget --quiet
+!pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html --quiet
+
+%cd /content/lama
+
+!curl -L $(yadisk-direct https://disk.yandex.ru/d/ouP6l8VJ0HpMZg) -o big-lama.zip
+!unzip big-lama.zip
+
+!pip uninstall opencv-python-headless -y --quiet
+!pip install opencv-python-headless==4.1.2.30 --quiet
+
+
+print('\n> Init mask-drawing code')
+import base64, os
+from IPython.display import HTML, Image
+from google.colab.output import eval_js
+from base64 import b64decode
+import matplotlib.pyplot as plt
+import numpy as np
+import wget
+from shutil import copyfile
+import shutil
+
+
+
+canvas_html = """
+<style>
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+<canvas1 width=%d height=%d>
+</canvas1>
+<canvas width=%d height=%d>
+</canvas>
+
+<button class="button">Finish</button>
+<script>
+var canvas = document.querySelector('canvas')
+var ctx = canvas.getContext('2d')
+
+var canvas1 = document.querySelector('canvas1')
+var ctx1 = canvas.getContext('2d')
+
+
+ctx.strokeStyle = 'red';
+
+var img = new Image();
+img.src = "data:image/%s;charset=utf-8;base64,%s";
+console.log(img)
+img.onload = function() {
+  ctx1.drawImage(img, 0, 0);
+};
+img.crossOrigin = 'Anonymous';
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+ctx.lineWidth = %d
+var button = document.querySelector('button')
+var mouse = {x: 0, y: 0}
+
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft
+  mouse.y = e.pageY - this.offsetTop
+})
+canvas.onmousedown = ()=>{
+  ctx.beginPath()
+  ctx.moveTo(mouse.x, mouse.y)
+  canvas.addEventListener('mousemove', onPaint)
+}
+canvas.onmouseup = ()=>{
+  canvas.removeEventListener('mousemove', onPaint)
+}
+var onPaint = ()=>{
+  ctx.lineTo(mouse.x, mouse.y)
+  ctx.stroke()
+}
+
+var data = new Promise(resolve=>{
+  button.onclick = ()=>{
+    resolve(canvas.toDataURL('image/png'))
+  }
+})
+</script>
+"""
+
+def draw(imgm, filename='drawing.png', w=400, h=200, line_width=1):
+  display(HTML(canvas_html % (w, h, w,h, filename.split('.')[-1], imgm, line_width)))
+  data = eval_js("data")
+  binary = b64decode(data.split(',')[1])
+  with open(filename, 'wb') as f:
+    f.write(binary)
+
+
+import cv2 as cv
+def obremover():
+  fname = None
+  if fname is None:
+    from google.colab import files
+    files = files.upload()
+    fname = list(files.keys())[0]
+  else:
+    fname = wget.download(fname)
+  shutil.rmtree('./data_for_prediction', ignore_errors=True)
+  !mkdir data_for_prediction
+
+  copyfile(fname, f'./data_for_prediction/{fname}')
+  os.remove(fname)
+  fname = f'./data_for_prediction/{fname}'
+
+  image64 = base64.b64encode(open(fname, 'rb').read())
+  image64 = image64.decode('utf-8')
+
+  print(f'Will use {fname} for inpainting')
+  import matplotlib.pyplot as plt
+  img = np.array(plt.imread(f'{fname}')[:,:,:3])
+
+  draw(image64, filename=f"./{fname.split('.')[1]}_mask.png", w=img.shape[1], h=img.shape[0], line_width=0.04*img.shape[1])
+ 
+  with_mask = np.array(plt.imread(f"./{fname.split('.')[1]}_mask.png")[:,:,:3])
+  mask = (with_mask[:,:,0]==1)*(with_mask[:,:,1]==0)*(with_mask[:,:,2]==0)
+  plt.imsave(f"./{fname.split('.')[1]}_mask.png",mask, cmap='gray')
+  img = np.array(plt.imread(f'{fname}')[:,:,:3])
+
+  img = np.array((1-mask.reshape(mask.shape[0], mask.shape[1], -1))*plt.imread(fname)[:,:,:3])
+
+  print('Run inpainting')
+  if '.jpeg' in fname:
+    !PYTHONPATH=. TORCH_HOME=$(pwd) python3 bin/predict.py model.path=$(pwd)/big-lama indir=$(pwd)/data_for_prediction outdir=/content/output dataset.img_suffix=.jpeg > /dev/null
+  elif '.jpg' in fname:
+    !PYTHONPATH=. TORCH_HOME=$(pwd) python3 bin/predict.py model.path=$(pwd)/big-lama indir=$(pwd)/data_for_prediction outdir=/content/output  dataset.img_suffix=.jpg > /dev/null
+  elif '.png' in fname:
+    !PYTHONPATH=. TORCH_HOME=$(pwd) python3 bin/predict.py model.path=$(pwd)/big-lama indir=$(pwd)/data_for_prediction outdir=/content/output  dataset.img_suffix=.png > /dev/null
+  else:
+    print(f'Error: unknown suffix .{fname.split(".")[-1]} use [.png, .jpeg, .jpg]')
+
+  plt.rcParams['figure.dpi'] = 200
+  plt.imshow(plt.imread(f"/content/output/{fname.split('.')[1].split('/')[2]}_mask.png"))
+  # _=plt.axis('off')
+  # _=plt.title('inpainting result')
+  plt.show()
+  fname = None
+
+  return cv.imread(r"/content/output/{fname.split('.')[1].split('/')[2]}_mask.png")
